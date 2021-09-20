8027a29de310a03bbe7bae755cc089b3164d43d8
张文畅 <715517322@qq.com>
2021-09-20 10:52:47 +0800

    http-server提交

---
+++ node-api/http-server.js
@@ -0,0 +1,26 @@
+(async () => {
+  const http = require('http');
+  const fs = require('fs');
+  const { promisify } = require('util');
+  const readFile = promisify(fs.readFile);
+
+  http
+    .createServer((request, response) => {
+      const { url, method } = request;
+      if (url === '/' && method === 'GET') {
+        try {
+          const data = await readFile('index.html');
+          response.statusCode = 200;
+          response.setHeader('Content-Type', 'text/html');
+          response.end(data);
+        } catch (err) {
+          response.statusCode = 500;
+          response.setHeader('Content-Type', 'text/plain;charset=urt-8');
+          response.end('500 服务器内部错误');
+        }
+      }
+    })
+    .listen(3000, () => {
+      console.log('服务器启动成功，端口 3000');
+    });
+})();
